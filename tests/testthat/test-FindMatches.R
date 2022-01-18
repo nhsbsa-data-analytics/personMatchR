f754a3a21fb67ed9e08222444b24567a7063e75d
Steven Buckley <steven.buckley@nhs.net>
2022-01-18 18:38:43 +0000

    clean test-FindMatches.R
    removed obsolete code

--- tests/testthat/test-FindMatches.R
+++ tests/testthat/test-FindMatches.R
@@ -3,9 +3,7 @@ library(dplyr)
 
 test_that("Sample test dataset output", {
 
-  #test_run <- FindMatches(readRDS("./tests/test_dataset_a.rds"), readRDS("./tests/test_dataset_b.rds"))
   test_run <- FindMatches(readRDS("../test_dataset_a.rds"), readRDS("../test_dataset_b.rds"))
-  #expected_results <- readRDS("./tests/test_match_results.rds")
   expected_results <- readRDS("../test_match_results.rds")
 
   expect_equal(dplyr::all_equal(test_run, expected_results), TRUE)
