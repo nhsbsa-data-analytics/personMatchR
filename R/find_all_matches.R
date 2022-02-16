d9332ce1ab0410ac05612c3e30610abc781a8869
AdnanS <adnan.shroufi@nhs.net>
2022-02-16 22:01:03 +0000

    find_matches_edit
    Find matches function edited to accept column names for primary_df and lookup_df

--- R/find_all_matches.R
+++ R/find_all_matches.R
@@ -13,161 +13,220 @@ df1 = read.csv("C:/Users/ADNSH/Desktop/TEST_DATA_1000000.csv") %>%
 df2 = read.csv("C:/Users/ADNSH/Desktop/TEST_DATA_1000000.csv") %>%
   sample_n(100000)
 
+df2 <- df2 %>%
+  rename(
+    ID_TWO = ID,
+    FORENAME_TWO = FORENAME,
+    SURNAME_TWO = SURNAME,
+    DOB_TWO = DOB,
+    POSTCODE_TWO = POSTCODE
+  )
 
-find_all_matches = function(df1, df2){
 
-  # Function Start
-  print(paste0("Cleaning Start: ", Sys.time()))
+find_all_matches = function(
+  df_one, id_one, forename_one, surname_one, dob_one, postcode_one,
+  df_two, id_two, forename_two, surname_two, dob_two, postcode_two
+  ){
 
   # Initial format of data
-  df1 <- df1 %>%
-    dplyr::select(ID, SURNAME, FORENAME, DOB, POSTCODE) %>%
-    format_dob() %>%
-    format_name() %>%
-    format_postcode()
+  df_one <- df_one %>%
+    dplyr::select(
+      {{ id_one }},
+      {{ forename_one }},
+      {{ surname_one }},
+      {{ dob_one }},
+      {{ postcode_one }}
+      ) %>%
+    format_dob(df = ., dob = {{ dob_one }}) %>%
+    format_name(df = ., name = {{ forename_one }}) %>%
+    format_name(df = ., name = {{ surname_one }}) %>%
+    format_postcode(., id = {{ id_one }}, postcode = {{ postcode_one }}) %>%
+    calc_permutations(
+      df = .,
+      forename = {{ forename_one }},
+      surname = {{ surname_one }},
+      postcode = {{ postcode_one }},
+      dob = {{ dob_one }}
+      )
 
   # Initial format of data
-  df2 <- df2 %>%
-    dplyr::select(ID, SURNAME, FORENAME, DOB, POSTCODE) %>%
-    format_dob() %>%
-    format_name() %>%
-    format_postcode()
-
-  # Function Start
-  print(paste0("Cleaning End: ", Sys.time()))
+  df_two <- df_two %>%
+    dplyr::select(
+      {{ id_two }},
+      {{ forename_two }},
+      {{ surname_two }},
+      {{ dob_two }},
+      {{ postcode_two }}
+    ) %>%
+    format_dob(df = ., dob = {{ dob_two }}) %>%
+    format_name(df = ., name = {{ forename_two }}) %>%
+    format_name(df = ., name = {{ surname_two }}) %>%
+    format_postcode(., id = {{ id_two }}, postcode = {{ postcode_two }}) %>%
+    calc_permutations(
+      df = .,
+      forename = {{ forename_two }},
+      surname = {{ surname_two }},
+      postcode = {{ postcode_two }},
+      dob = {{ dob_two }}
+    )
 
   # identify exact matches
-  df_exact <- df1 %>%
+  exact <- df_one %>%
+    dplyr::select(
+      {{ id_one }},
+      {{ forename_one }},
+      {{ surname_one }},
+      {{ dob_one }},
+      {{ postcode_one }}
+    ) %>%
     # join where all fields match exactly
     dplyr::inner_join(
-      y = df2,
-      by = c(
-        "SURNAME" = "SURNAME",
-        "FORENAME" = "FORENAME",
-        "DOB" = "DOB",
-        "POSTCODE" = "POSTCODE"
-      ),
+      y = df_two %>%
+        dplyr::select(
+          {{ id_two }},
+          {{ forename_two }},
+          {{ surname_two }},
+          {{ dob_two }},
+          {{ postcode_two }}
+        ) %>%
+        rename(
+          {{ forename_one }} := {{ forename_two }},
+          {{ surname_one }} := {{ surname_two }},
+          {{ dob_one }} := {{ dob_two }},
+          {{ postcode_one }} := {{ postcode_two }}
+        ),
       keep = FALSE,
       na_matches = "never"
-    )
-
-  # Identify 'reverse name' exact matches
-  df_exact_rev <- df1 %>%
-    # join where all fields match exactly
-    dplyr::inner_join(
-      y = df2,
-      by = c(
-        "FORENAME" = "SURNAME",
-        "SURNAME" = "FORENAME",
-        "DOB" = "DOB",
-        "POSTCODE" = "POSTCODE"
-      ),
-      keep = FALSE,
-      na_matches = "never"
-    )
-
-  # Generate features for all exact matches
-  df_exact <- df_exact %>%
-    dplyr::bind_rows(df_exact_rev) %>%
+    ) %>%
+    # Generate features for all exact matches
     # by default all matches are an "exact" match with perfect scores
     dplyr::mutate(
+      {{ forename_two }} := {{ forename_one }},
+      {{ surname_two }} := {{ surname_one }},
+      {{ dob_two }} := {{ dob_one }},
+      {{ postcode_two }} := {{ postcode_one }},
       JW_SURNAME = 1,
       JW_FORENAME = 1,
       JW_POSTCODE = 1,
       ED_DOB = 0,
       MATCH_TYPE = 'Exact'
     ) %>%
-    # select only the key fields
-    dplyr::select(
-      ID.x, ID.y, JW_SURNAME, JW_FORENAME, JW_POSTCODE, ED_DOB, MATCH_TYPE
+    dplyr::select({{ id_one }}, {{ id_two }}, everything())
+
+  # Remove exact matches from primary df
+  df_one <- df_one %>%
+    dplyr::anti_join(y = exact %>% select({{ id_one }}))
+
+  # Permutation join function
+  perm_join <- function(df_one, df_two, id_one, id_two, perm_num){
+
+    # Convert integer to permutation column name
+    perm_num = sym(paste0("PERM", perm_num))
+
+    # Join on composite value
+    output <- dplyr::inner_join(
+      x = df_one %>% dplyr::select({{ id_one }}, perm_num),
+      y = df_two %>% dplyr::select({{ id_two }}, perm_num)
     )
 
-  # Identify a vector of records where an exact match has been identified
-  exact_match_list <- df_exact %>%
-    select(ID.x) %>%
-    distinct() %>%
-    pull()
+    # Return output
+    return(output)
+  }
 
-  # limit to only records not already matched based on an exact match
-  df1 <- df1 %>%
-    dplyr::filter(!ID %in% exact_match_list)
-
-  # Vector of values for lapply
-  chunk_size <- 2000000000 / nrow(df2)
-  chunk_num <- ceiling(nrow(df1) / chunk_size)
-  chunk_vec = rep(1:chunk_num, chunk_size)[1:nrow(df1)]
-
-  # Add Chunk Variable to Remaining DF
-  df1 <- df1 %>%
-    cbind(CHUNK = chunk_vec)
-
-  # Function for lapply
-  cross_match <- function(df1, df2, chunk){
-
-    # Filter One of Pre-Defined Subsets
-    df1 <- df1 %>%
-      dplyr::filter(CHUNK == chunk) %>%
-      dplyr::select(-CHUNK)
-
-    # Fuzzy Cross Join
-    cross_join = fuzzyjoin::stringdist_full_join(
-      x = df1,
-      y = df2,
-      by = "FORENAME",
-      method = "jw",
-      max_dist = 0.25
+  # Lapply all permutations into list then row-bind
+  id_pairs <- lapply(
+    X = 1:9,
+    FUN = perm_join,
+    df_one = df_one,
+    df_two = df_two,
+    id_one = {{ id_one }},
+    id_two = {{ id_two }}
     ) %>%
-      mutate(LV = stringdist::stringdist(DOB.x, DOB.y, "lv")) %>%
-      filter(LV <= 2)
+    dplyr::bind_rows() %>%
+    dplyr::select({{ id_one }}, {{ id_two }}) %>%
+    dplyr::distinct()
 
-    # Get Matches
-    matches <- cross_join %>%
-      dplyr::mutate(
-        JW_SURNAME = stringdist::stringsim(SURNAME.x, SURNAME.y, method = "jw"),
-        JW_FORENAME = stringdist::stringsim(FORENAME.x, FORENAME.y, method = "jw"),
-        JW_POSTCODE = stringdist::stringsim(POSTCODE.x, POSTCODE.y, method = "jw"),
-        ED_DOB = stringdist::stringdist(DOB.x, DOB.y, method = "lv")
+  # Remove Permutation info from matching dfs
+  df_one <- df_one %>%
+    dplyr::select(
+      {{ id_one }},
+      {{ forename_one }},
+      {{ surname_one }},
+      {{ dob_one }},
+      {{ postcode_one }}
+    )
+
+  # Remove Permutation info from matching dfs
+  df_two <- df_two %>%
+    dplyr::select(
+      {{ id_two }},
+      {{ forename_two }},
+      {{ surname_two }},
+      {{ dob_two }},
+      {{ postcode_two }}
+    )
+
+  # Join lookup back to original dfs
+  id_pairs <- id_pairs %>%
+    dplyr::inner_join(y = df_one) %>%
+    dplyr::inner_join(y = df_two)
+
+  # Get list of eligible dobs and forenames
+  eligible_dob_forename <- id_pairs %>%
+    dplyr::select(
+      {{ dob_one }},
+      {{ dob_two }},
+      {{ forename_one }},
+      {{ forename_two }}
+      ) %>%
+    dplyr::mutate(
+      ED_DOB = stringdist::stringdist(
+        {{ dob_one }}, {{ dob_two }}, method = 'lv'
+        ),
+      JW_FORENAME = stringdist::stringsim(
+        {{ forename_one }}, {{ forename_two }}, method = 'jw'
+        )
+    ) %>%
+    dplyr::filter(ED_DOB <= 2 & JW_FORENAME >= 0.75) %>%
+    distinct()
+
+  # Filter permutation output by feasible date-pair list
+  cross <- id_pairs %>%
+    dplyr::inner_join(eligible_dob_forename)
+
+  # Get Matches
+  match <- cross %>%
+    dplyr::mutate(
+      JW_SURNAME = stringdist::stringsim(
+        {{ surname_one }}, {{ surname_two }}, method = "jw"
+        ),
+      JW_POSTCODE = stringdist::stringsim(
+        {{ postcode_one }}, {{ postcode_two }}, method = "jw"
+        )
       ) %>%
       # limit to key fields and score matches
-      dplyr::select(ID.x, ID.y, JW_SURNAME, JW_FORENAME, JW_POSTCODE, ED_DOB) %>%
-      dplyr::mutate(MATCH_TYPE = case_when(
-        (JW_SURNAME == 1 & JW_FORENAME == 1 & JW_POSTCODE == 1 & ED_DOB == 0) ~ "Exact",
-        (JW_SURNAME == 1 & JW_FORENAME == 1 & ED_DOB == 0) ~ "Confident",
-        (JW_SURNAME == 1 & JW_FORENAME == 1 & JW_POSTCODE == 1 & ED_DOB <= 2) ~ "Confident",
-        (JW_FORENAME == 1 & JW_POSTCODE == 1 & ED_DOB == 0) ~ "Confident",
-        (JW_SURNAME == 1 & JW_FORENAME >= 0.75 & JW_POSTCODE == 1 & ED_DOB == 0) ~ "Confident",
-        (JW_SURNAME >= 0.85 & JW_FORENAME >= 0.75 & JW_POSTCODE >= 0.85 & ED_DOB <= 2) ~ "Confident",
+      dplyr::mutate(MATCH_TYPE = dplyr::case_when(
+        JW_SURNAME == 1 & JW_FORENAME == 1 & JW_POSTCODE == 1 & ED_DOB == 0 ~ "Exact",
+        JW_SURNAME == 1 & JW_FORENAME == 1 & ED_DOB == 0 ~ "Confident",
+        JW_SURNAME == 1 & JW_FORENAME == 1 & JW_POSTCODE == 1 & ED_DOB <= 2 ~ "Confident",
+        JW_FORENAME == 1 & JW_POSTCODE == 1 & ED_DOB == 0 ~ "Confident",
+        JW_SURNAME == 1 & JW_FORENAME >= 0.75 & JW_POSTCODE == 1 & ED_DOB == 0 ~ "Confident",
+        JW_SURNAME >= 0.85 & JW_FORENAME >= 0.75 & JW_POSTCODE >= 0.85 & ED_DOB <= 2 ~ "Confident",
         TRUE ~ "No Match"
-      )
+        )
       ) %>%
       # filter to only confident matches
       dplyr::filter(MATCH_TYPE != "No Match")
 
-    # Clean then return
-    gc()
-    return(matches)
-  }
-
-  # Function Start
-  print(paste0("Cross Match Start: ", Sys.time()))
-
-  # Get lapply results and bind rows
-  matches <- lapply(X = 1:chunk_num, FUN = cross_match, df1 = df1, df2 = df2) %>%
-    dplyr::bind_rows()
-
-  # Function End
-  print(paste0("Cross Match End: ", Sys.time()))
-
   # combine the exact and confident matches
-  matches <- matches %>%
-    rbind(df_exact) %>%
-    dplyr::group_by(ID.x) %>%
-    dplyr::mutate(MATCH_COUNT = n()) %>%
-    dplyr::ungroup() %>%
-    dplyr::select(ID.x, MATCH_COUNT, ID.y, MATCH_TYPE)
+  matches <- match %>%
+    rbind(exact)
 
   # return result
   return(matches)
 }
 
-output <- find_all_matches(df1, df2)
+output <- find_all_matches(
+  df1, ID, FORENAME, SURNAME, DOB, POSTCODE,
+  df2, ID_TWO, FORENAME_TWO, SURNAME_TWO, DOB_TWO, POSTCODE_TWO
+)
--- R/format_functions.R
+++ R/format_functions.R
@@ -12,16 +12,13 @@
 #'
 #' @examples
 #' format_name(str_name)
-format_name <- function(df){
+format_name <- function(df, name){
 
   df %>%
     mutate(
       # Remove non-alpha chars and convert emtpy string to NA
-      FORENAME = toupper(gsub("[^[:alpha:]]", "", FORENAME)),
-      FORENAME = ifelse(FORENAME == "", NA, FORENAME),
-      SURNAME = toupper(gsub("[^[:alpha:]]", "", SURNAME)),
-      FORENAME = ifelse(FORENAME == "", NA, FORENAME),
-      SURNAME = ifelse(SURNAME == "", NA, SURNAME)
+      {{ name }} := toupper(gsub("[^[:alpha:]]", "", {{ name }})),
+      {{ name }} := ifelse({{ name }} == "", NA, {{ name }})
     )
 }
 
@@ -38,21 +35,21 @@ format_name <- function(df){
 #'
 #' @examples
 #' format_dob(dob)
-format_dob <- function(df){
+format_dob <- function(df, dob){
 
   df %>%
     mutate(
       # Remove hyphens and upper case
-      DOB = gsub('/', '-', DOB),
-      DOB = toupper(DOB),
+      {{ dob }} := gsub('/', '-', {{ dob }}),
+      {{ dob }} := toupper({{ dob }}),
       # Generate various potential dates
-      DOB_V1 = as.Date(DOB, format = '%d-%B-%y'),
-      DOB_V2 = as.Date(DOB, format = '%Y-%m-%d'),
+      DOB_V1 = as.Date({{ dob }}, format = '%d-%B-%y'),
+      DOB_V2 = as.Date({{ dob }}, format = '%Y-%m-%d'),
       # Find first non-NA date then format as string
-      DOB = dplyr::coalesce(DOB_V1, DOB_V2),
-      DOB = format(DOB, format = '%Y%m%d'),
+      {{ dob }} := dplyr::coalesce(DOB_V1, DOB_V2),
+      {{ dob }} := format({{ dob }}, format = '%Y%m%d'),
       # Empty strings as NA
-      DOB = ifelse(DOB == "", NA, DOB)
+      {{ dob }} := ifelse({{ dob }} == "", NA, {{ dob }})
     ) %>%
     select(-c(DOB_V1, DOB_V2))
 }
@@ -69,7 +66,7 @@ format_dob <- function(df){
 #'
 #' @examples
 #' format_postcode(df)
-format_postcode = function(df){
+format_postcode = function(df, id, postcode){
 
   # Helper function to replace numbers for characters
   replace_number_for_char = function(x){
@@ -84,12 +81,11 @@ format_postcode = function(df){
   # Process df
   df %>%
     mutate(
-      POSTCODE = ifelse(POSTCODE == "", NA, POSTCODE),
-      POSTCODE = toupper(stringr::str_replace_all(POSTCODE, "[^[:alnum:]]", ""))
+      {{ postcode }} := toupper(gsub("[^[:alnum:]]", "", {{ postcode }}))
     ) %>%
-    tidytext::unnest_characters(., 'CHAR', 'POSTCODE', to_lower = F) %>%
+    tidytext::unnest_characters(., 'CHAR', {{ postcode }}, to_lower = F) %>%
     mutate(NUM = ifelse(grepl("[1-9]", CHAR), 1, 0)) %>%
-    group_by(ID) %>%
+    group_by({{ id }}) %>%
     # Determine total chars and char position per postcode
     mutate(
       ROW = row_number(),
@@ -122,11 +118,72 @@ format_postcode = function(df){
       )
     ) %>%
     select(-c(NUM, LEN)) %>%
-    group_by(ID) %>%
+    group_by({{ id }}) %>%
     # Paste split chars into single column
-    mutate(POSTCODE = paste(CHAR, collapse = "")) %>%
+    mutate({{ postcode }} := paste(CHAR, collapse = "")) %>%
     ungroup() %>%
     # Select and distinct to remove split-char rows
-    select(ID, FORENAME, SURNAME, DOB, POSTCODE) %>%
+    select(-c(CHAR, ROW)) %>%
     distinct()
+}
+
+#' Calculates 9 permutations for primary-lookup join prior to match scoring
+#'
+#' @param df A df to be formatted
+#' @param forename forename column name
+#' @param surname surname column name
+#' @param postcode postcode column name
+#' @param dob DOB column name
+#'
+#' @return A df with 5 'basic' join permutations added
+#'
+#' @export
+#'
+#' @examples
+#' calc_permutations(df, forename, surname, postcode, dob)
+calc_permutations <- function(df, forename, surname, postcode, dob){
+
+  df %>%
+    mutate(
+      # Perm 1-3 require full match of dob with 2 of forename, surname, postcode
+      PERM1 = paste0({{ forename }}, {{ surname }}, {{ dob }}),
+      PERM2 = paste0({{ forename }}, {{ postcode }}, {{dob}}),
+      PERM3 = paste0({{ surname }}, {{ postcode }}, {{ dob }}),
+      # First char forename, plus 4 chars of surname and postcode
+      PERM4 = paste0(
+        substr({{ forename }}, 1, 1),
+        substr({{ surname }}, 1, 4),
+        substr({{ postcode }}, 1, 4)
+      ),
+      # First 3 chars forename, plus 3 chars of surname and postcode
+      PERM5 = paste0(
+        substr({{ forename }}, 1, 3),
+        substr({{ surname }}, 1, 2),
+        substr({{ postcode }}, 1, 2)
+      ),
+      # Last 3 chars forename, plus 3 chars of surname and postcode
+      PERM6 = paste0(
+        substr({{ forename }}, nchar({{ forename }})-2, 3),
+        substr({{ surname }}, 1, 2),
+        substr({{ postcode }}, 1, 2)
+      ),
+      # First 3 consonants, plus 3 chars of surname and postcode
+      PERM7 = paste0(
+        substr(gsub('[AEIOU]', '', {{ forename}}), 1, 3),
+        substr({{ surname }}, 1, 2),
+        substr({{ postcode }}, 1, 2)
+      ),
+      # All consonants, plus 4 chars of surname and postcode
+      PERM8 = paste0(
+        gsub('[AEIOU]', '', {{ forename}}),
+        substr({{ surname }}, 1, 2),
+        substr({{ postcode }}, 1, 2)
+      ),
+      # All vowels, plus 3 chars of surname and postcode
+      PERM9 = paste0(
+        gsub('[B-DF-HJ-NP-TV-Z]', '', {{ forename }}),
+        substr({{ surname }}, 1, 3),
+        substr({{ postcode }}, 1, 3)
+      )
+    )
 }
