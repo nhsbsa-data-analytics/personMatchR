0060bd99661176d35f92903f2f263a3dcb169f61
AdnanS <adnan.shroufi@nhs.net>
2022-02-19 07:33:57 +0000

    simplifying_results

--- R/calc_db_function_performance_test.R
+++ R/calc_db_function_performance_test.R
@@ -17,7 +17,7 @@ eib_db <- con %>%
   dplyr::tbl(from = dbplyr::in_schema("STBUC", "INT617_TMP_EIBSS"))
 
 #-------------------------------------------------------------------------------
-# Part One: ~10m matches
+# Part One: Custom JW Calculation ~10m matches
 
 # Just select forename
 eib <- eib_db %>%
@@ -57,53 +57,16 @@ results_two <- all %>%
   collect()
 Sys.time()
 
-#-------------------------------------------------------------------------------
-# Part Two: ~10m matches
-
-# Just select forename
-eib <- eib_db %>%
-  select(FORENAME_ONE = FORENAME) %>%
-  mutate(TMP = 1)
-
-# Just select forename
-pds <- pds_db %>%
-  select(FORENAME_TWO = FORENAME) %>%
-  mutate(
-    TMP = 1,
-    ID = row_number(FORENAME_TWO)
-  ) %>%
-  filter(ID <= 1000000) %>%
-  select(-ID)
-
-# Full join
-all <- eib %>%
-  full_join(pds) %>%
-  distinct()
-
-# 10.7m distinct forename-pairs
-all %>% tally()
-
-# Time for 'normal' JW calculation: 10.7m = 36mins
-Sys.time()
-results_one <- all %>%
-  mutate(JW = UTL_MATCH.JARO_WINKLER(FORENAME_ONE, FORENAME_TWO)) %>%
-  filter(JW >= 0.75) %>%
-  collect()
-Sys.time()
-
-# Time for custom JW calculation: 10.7m = 16mins
-Sys.time()
-results_two <- all %>%
-  calc_db_jw_threshold(., FORENAME_ONE, FORENAME_TWO, 0.75, "JW") %>%
-  collect()
-Sys.time()
+# Disconnect
+DBI::dbDisconnect(con)
 
 #-------------------------------------------------------------------------------
-# Summary
+# Part Two: Custom JW function - Summary
 
 # 1. Both methods have same output
-# 2. Method 2 (in this instance) took ~44% of the time of Method 1
+# 2. 10.7m - Method 2 (in this instance) took ~44% of the time of Method 1
 # 3. With smaller numbers, the difference is less
-# 4. With larger numbers, difference may be more
+# 4. With very small number, diff will at some point become negligible
+# 5. With larger numbers, difference may be more
 
 #-------------------------------------------------------------------------------
